907310808c672924e1ca4d4f8fde2379d63680e6
Sungbin Jo <pcr910303@icloud.com>
2019-08-03 00:38:24 +0900

    Cleanup repo

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
-import 'typeface-roboto';
+import "typeface-roboto";
 
-const React = require("react");
-const ReactDOM = require("react-dom");
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-import Main from './main.jsx'
+import Main from "./main.jsx";
 
 const AppRouter = () => (
   <div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,143 +1,148 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-import { makeStyles } from '@material-ui/core/styles';
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import './main.css';
+import React from "react";
 
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-  },
-  grid: {
-    flexGrow: 1,
-  },
-}));
+import { makeStyles } from "@material-ui/core/styles";
+
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles({
+    root: {
+        width: "100%",
+    },
+    grid: {
+        flexGrow: 1,
+    },
+    wrapper: {
+        display: "grid",
+        gridTemplateColumns: "repeat(6, 1fr)",
+        gridTemplateRows: [100, 100, 100]
+    }
+});
 
 const lectureList = [
-  { subject: "수학", lecture: ["수학 I", "수학 II", "수학 III"] },
-  { subject: "물리", lecture: ["물리 I", "물리 II"] }
+    { subject: "수학", lecture: ["수학 I", "수학 II", "수학 III"] },
+    { subject: "물리", lecture: ["물리 I", "물리 II"] }
 ];
 
 const curLecture = [];
 const curTimetable = [
-  ["흠", "월요일", "화요일", "수요일", "목요일", "금요일"],
-  ["1교시", "", "", "", "", ""],
-  ["2교시", "", "", "", "", ""],
-  ["3교시", "", "", "", "jjjj", ""],
-  ["4교시", "", "", "", "", ""],
-  ["5교시", "", "", "", "", ""],
-  ["6교시", "", "", "", "", ""],
-  ["7교시", "", "", "", "", ""],
-]
+    ["흠", "월요일", "화요일", "수요일", "목요일", "금요일"],
+    ["1교시", "", "", "", "", ""],
+    ["2교시", "", "", "", "", ""],
+    ["3교시", "", "", "", "", ""],
+    ["4교시", "", "", "", "", ""],
+    ["5교시", "", "", "", "", ""],
+    ["6교시", "", "", "", "", ""],
+    ["7교시", "", "", "", "", ""],
+];
 
 function addLecture(props) {
-  if (curLecture.length > 0) {
-    let i = 0;
-    for (i = 0; i < curLecture.length; i++) {
-      if (curLecture[i] == props) { break; }
+    if (curLecture.length > 0) {
+        let i = 0;
+        for (i = 0; i < curLecture.length; i++) {
+            if (curLecture[i] == props) { break; }
+        }
+        if (i === curLecture.length) {
+            curLecture.push(props);
+        }
     }
-    if (i === curLecture.length) {
-      curLecture.push(props);
+    else {
+        curLecture.push(props);
     }
-  }
-  else {
-    curLecture.push(props);
-  }
-  console.log(curLecture);
+    console.log(curLecture);
 }
 
 function onDragEnd(props) {
-  if (!props.destination) {
-    return;
-  }
+    if (!props.destination) {
+        return;
+    }
 }
 
 function Test() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Paper>
-      <GridList cellHeight='auto'>
-        <GridListTile>
-          {lectureList.map(subject => {
-            return (
-              <ExpansionPanel>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                  {subject['subject']}
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                  <List className={classes.root}>
-                    {subject['lecture'].map(lecture => {
-                      return (
-                        <>
-                          <ListItem button={true} onClick={ addLecture(lecture) }>
-                            {lecture}
-                          </ListItem>
-                          <Divider />
-                        </>
-                      );
+    return (
+        <Paper>
+            <GridList cellHeight="auto">
+                <GridListTile>
+                    {lectureList.map(subject => {
+                        return (
+                            <ExpansionPanel>
+                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                    {subject["subject"]}
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails>
+                                    <List className={classes.root}>
+                                        {subject["lecture"].map(lecture => {
+                                            return (
+                                                <>
+                                                    <ListItem button={true} onClick={ addLecture(lecture) }>
+                                                        {lecture}
+                                                    </ListItem>
+                                                    <Divider />
+                                                </>
+                                            );
+                                        })}
+                                    </List>
+                                </ExpansionPanelDetails>
+                            </ExpansionPanel>
+                        );
                     })}
-                  </List>
-                </ExpansionPanelDetails>
-              </ExpansionPanel>
-            );
-          })}
-        </GridListTile>
-        <DragDropContext onDragEnd={e => { console.log(e); onDragEnd(e);} }>
-          <GridListTile>
-            <Droppable droppableId="curlecture" type="list">
-              {(provided, snapshot) => {
-                return (
-                  <List ref={provided.innerRef}>
-                    {curLecture.map(lecture => {
-                      return (
-                        <>
-                          <ListItem>
-                            {lecture}
-                          </ListItem>
-                          <Divider />
-                        </>
-                      );
-                    })}
-                  </List>
-                );
-              }}
-            </Droppable>
-          </GridListTile>
-          <div className={classes.grid}>
-            <Grid container>
-              {curTimetable.map(period => {
-                return (
-                  <div>
-                    {period.map(day => {
-                      return (
-                        <Grid>
-                          <Paper>{day}</Paper>
+                </GridListTile>
+                <DragDropContext onDragEnd={e => { console.log(e); onDragEnd(e);} }>
+                    <GridListTile>
+                        <Droppable droppableId="curlecture" type="list">
+                            {(provided, snapshot) => {
+                                return (
+                                    <List ref={provided.innerRef}>
+                                        {curLecture.map(lecture => {
+                                            return (
+                                                <>
+                                                    <ListItem>
+                                                        {lecture}
+                                                    </ListItem>
+                                                    <Divider />
+                                                </>
+                                            );
+                                        })}
+                                    </List>
+                                );
+                            }}
+                        </Droppable>
+                    </GridListTile>
+                    <div className={classes.grid}>
+                        <Grid container>
+                            {curTimetable.map(period => {
+                                return (
+                                    <div>
+                                        {period.map(day => {
+                                            return (
+                                                <Grid>
+                                                    <Paper>{day}</Paper>
+                                                </Grid>
+                                            );
+                                        })}
+                                    </div>
+                                );
+                            })}
                         </Grid>
-                      );
-                    })}
-                  </div>
-                );
-              })}
-            </Grid>
-          </div>
-        </DragDropContext>
-      </GridList>
-    </Paper>
-  );
+                    </div>
+                </DragDropContext>
+            </GridList>
+        </Paper>
+    );
 }
 
 export default Test;
